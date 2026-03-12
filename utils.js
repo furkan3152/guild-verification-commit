feb8950ac440225c97454217609a600daf36c45c
furkan3152 <70746444+furkan3152@users.noreply.github.com>
2026-03-12 13:44:47 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T10:44:29.948Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-12T10:44:46.640Z
+Commit: Resolve null pointer exception in user service
