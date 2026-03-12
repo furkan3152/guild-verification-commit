a8721ccf5059f324aa4220f9691c25f30e745f26
furkan3152 <70746444+furkan3152@users.noreply.github.com>
2026-03-12 13:44:14 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T10:43:54.628Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-12T10:44:13.193Z
+Commit: Fix CSS styling bug on mobile devices
