4d8d62d6c74473539e542be926675bb1754ded73
furkan3152 <70746444+furkan3152@users.noreply.github.com>
2026-03-12 13:45:19 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T10:45:02.652Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-12T10:45:18.553Z
+Commit: Fix memory leak in event handlers
