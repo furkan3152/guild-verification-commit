999e97fca0a94f3631ae9b9b98c99c373f2b6c56
furkan3152 <70746444+furkan3152@users.noreply.github.com>
2026-03-12 13:43:55 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T10:43:11.542Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-12T10:43:54.628Z
+Commit: Patch API endpoint error handling
