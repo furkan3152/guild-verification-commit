dc70bdcea2edbb2cf95b56b14ba8d3dddf0a0c25
furkan3152 <70746444+furkan3152@users.noreply.github.com>
2026-03-12 13:43:57 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T10:43:47.755Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-12T10:43:56.072Z
+Commit: Resolve timezone issues in date handling
